de1d01857593c851c2ba9f284e1289921f7ff328
tbrook <tbrook.net@gmail.com>
2017-04-07 00:18:39 +0900

    zplug の設定の変更

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,22 +1,20 @@
 
 zplug "zplug/zplug"
 
-zplug "$ZDOTDIR/conf.d", from:local
+
+zplug "${ZDOTDIR}/conf.d", from:local
+
 
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-history-substring-search", defer:3
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
 
 zplug "zsh-users/zsh-autosuggestions"
+
+
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
 
 zplug "peco/peco", \
 as:command, \
 from:gh-r, \
 frozen:1
-
-
-
-
-
-
-
