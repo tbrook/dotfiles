0f815ac6814480046742d33dc2899d90d50f5c17
tbrook <tbrook.net@gmail.com>
2017-04-07 00:17:00 +0900

    キーバインドの変更

--- .zsh/conf.d/bindkey.zsh
+++ .zsh/conf.d/bindkey.zsh
@@ -2,8 +2,8 @@
 bindkey -e
 
 
-bindkey -M emacs "^[[A" history-incremental-search-forward
-bindkey -M emacs "^[[B" history-incremental-search-backward
+bindkey -M emacs "^[[A" history-incremental-search-backward
+bindkey -M emacs "^[[B" history-incremental-search-forward
 bindkey -M emacs "^[[C" forward-word
 bindkey -M emacs "^[[D" backward-word
 
