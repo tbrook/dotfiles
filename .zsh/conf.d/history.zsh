ee8b109eb4bcba6cc100398c87a0dc8dc07e940b
tbrook <tbrook.net@gmail.com>
2017-04-09 05:10:14 +0900

    環境変数 HISTFILE の値を変更

--- .zsh/conf.d/history.zsh
+++ .zsh/conf.d/history.zsh
@@ -1,5 +1,13 @@
 
-export HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
+hist_file="${SOFTWARE_PATH}/var/.zsh_history"
+
+if [[ ! -f "${hist_file}" ]]; then
+    hist_file="${HOME}/.zsh_history"
+fi
+
+export HISTFILE="${hist_file}"
+
+
 export HISTSIZE=100000000
 export SAVEHIST=100000000
 
