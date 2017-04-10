3fd89b7ee11a2cb51d1fd3d5334122ee1bee41c1
tbrook <tbrook.net@gmail.com>
2017-04-10 14:45:36 +0900

    zsh の history (履歴) の設定

--- .zsh/conf.d/history.zsh
+++ .zsh/conf.d/history.zsh
@@ -12,6 +12,20 @@ export HISTSIZE=100000000
 export SAVEHIST=100000000
 
 
+setopt hist_ignore_dups     # ignore duplication command history list
+setopt share_history        # share command history data
+
+
+# http://news.mynavi.jp/column/zsh/004/
+# http://mollifier.hatenablog.com/entry/20090318/1237302243
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^n" history-beginning-search-forward-end
+
+
+
 # Local variables:
 # mode: shell-script
 # End:
