1e93a3ef0aeb80efff865951a500cad20d8110df
tbrook <tbrook.net@gmail.com>
2017-04-10 17:07:42 +0900

    new file:   .zsh/rc.d/homeshick.zsh

---
+++ .zsh/rc.d/homeshick.zsh
@@ -0,0 +1,15 @@
+
+# homeshick, homesick
+[[ -f ~/.homesick/repos/homeshick/homeshick.sh ]] && . ~/.homesick/repos/homeshick/homeshick.sh
+
+
+# fpath=($HOME/.homesick/repos/homeshick/completions $fpath)
+[[ -f ~/.homesick/repos/homeshick/completions/homeshick-completion.bash ]] && . ~/.homesick/repos/homeshick/completions/homeshick-completion.bash
+
+# 
+:
+
+
+# Local variables:
+# mode: shell-script
+# End:
