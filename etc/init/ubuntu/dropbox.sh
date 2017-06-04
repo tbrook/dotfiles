632ec8bd83e4f4dc8f9ef339f60f91bc3642c7f4
tbrook <tbrook.net@gmail.com>
2017-06-05 02:02:30 +0900

    ubuntu 用の dropbox のインストーラー。 未検証

---
+++ etc/init/ubuntu/dropbox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Stop script if errors occur
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+
+# https://www.dropbox.com/install-linux
+# https://www.xmisao.com/2013/12/17/debian-64bit-dropbox-install.html
+
+
+cd "$HOME" && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+# CUI だと認証用 の URL が表示される
+"$HOME/.dropbox-dist/dropboxd"
+
+sudo -v
+
+sudo apt-get install nautilus-dropbox
+
+
+# dropbox start の前に
+# pushd ~/; cat /path/to/exclude-list | xargs dropbox exclude add; popd
+# を行う
