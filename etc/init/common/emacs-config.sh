13cd00c57975fb439ed3c7eb7bdad1ca6ec9fceb
tbrook <tbrook.net@gmail.com>
2017-06-18 18:08:19 +0900

    eww-bookmarks の配置を設定

--- etc/init/common/emacs-config.sh
+++ etc/init/common/emacs-config.sh
@@ -6,4 +6,29 @@ trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 
 
+
+tb.conf.eww () {
+    eww_bm_name="eww-bookmarks"
+
+    eww_bm_file="${SOFTWARE_PATH}/var/emacs/${eww_bm_name}"
+
+    mkdir -p "${eww_bm_file%/*}"
+
+    if [[ -f "$eww_bm_file" ]]; then
+	if builtin pushd "${HOME}/.emacs.d" > /dev/null; then
+	    if [[ -e "$eww_bm_name" ]]; then
+
+		mv --backup=numbered "$eww_bm_name" "${eww_bm_name}.orig"
+	    fi
+
+	    ln -s "$eww_bm_file" .
+
+	    builtin popd > /dev/null
+	fi
+    fi
+}
+
+
 git clone ssh://git@bitbucket.org/tbrook/dot.emacs.d.git "${HOME}/.emacs.d"
+
+tb.conf.eww
