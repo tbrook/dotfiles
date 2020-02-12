4da5db18a03bf00fd177e27d7276fcd676cb3459
tbrook <tbrook.src@gmail.com>
2020-02-13 05:01:31 +0900

    zsh の bindkey に Ctrl+矢印 を追加。

--- .zsh/rc.d/bindkey.zsh
+++ .zsh/rc.d/bindkey.zsh
@@ -7,6 +7,9 @@ bindkey -M emacs "^[[B" history-incremental-search-forward
 bindkey -M emacs "^[[C" forward-word
 bindkey -M emacs "^[[D" backward-word
 
+bindkey -M emacs "^[[1;5C" forward-word
+bindkey -M emacs "^[[1;5D" backward-word
+
 
 # Local variables:
 # mode: shell-script
