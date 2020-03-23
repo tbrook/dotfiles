886b81712a66d3e4ab59cbbf2739262744f790ae
tbrook <tbrook.src@gmail.com>
2020-03-23 10:53:08 +0900

    etc/init/ubuntu-package/dropbox.sh を修正

--- etc/init/ubuntu-package/dropbox.sh
+++ etc/init/ubuntu-package/dropbox.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 
+#
+# https://www.dropbox.com/install-linux
+#
+
+
 # Stop script if errors occur
 trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 
 
-# https://www.dropbox.com/install-linux
-# https://www.xmisao.com/2013/12/17/debian-64bit-dropbox-install.html
+. "${DOTPATH:-${HOME}/.dotfiles}"/etc/lib/vital.sh
 
 
-cd "$HOME" && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+dst_dir="${HOME}"
+cmd_dropbox="${dst_dir}/dropbox.py"
 
 
-sudo apt-get -y install nautilus-dropbox
+if builtin pushd "${dst_dir}" > /dev/null; then
+    wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+    e_newline && e_done "Install ${dst_dir}/.dropbox-dist"
+
+    wget --output-document="${cmd_dropbox}" 'https://www.dropbox.com/download?dl=packages/dropbox.py'
+    chmod 755 "${cmd_dropbox}"
+    e_newline && e_done "Install ${cmd_dropbox}"
+
+    builtin popd > /dev/null
+fi
